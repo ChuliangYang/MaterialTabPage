11c4826bae0cd1abc4bc799586764a097a8add9f
CarlosYang <mrcarlosyang@gmail.com>
2018-08-12 21:13:23 -0400

    change name

--- app/src/main/java/com/me/cl/materialtabhost/MVVM/data/transform/protocol/CityTransformer.kt
+++
@@ -1,7 +0,0 @@
-package com.me.cl.materialtabhost.MVVM.data.transform.protocol
-
-import com.me.cl.materialtabhost.bean.entities.City
-
-interface CityTransformer {
-    fun divideIntoTwoList(totalList: List<City>): Array<List<City>>
-}
